955aff4acac6f8cf9d91b87eee6a8cd7a8d7d480
chikamasuda <c.m.062820@gmail.com>
2021-12-24 15:39:43 +0900

    ホーム画面のレイアウト変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,29 +25,31 @@
         <h2 class="title pb-2 title-border pt-3">
             人気急上昇イベント(直近24時間)
         </h2>
-        @foreach($lists as $list)
-        <ul class="list-unstyled event-card pb-4 pt-4 pl-4 pr-4">
-            <li class="font-weight-bold card-title"><a href="{{ $list->event->url }}" target="_blank" class="text-body">{{ $list->event->title }}</a></li>
-            <li class="list-unstyled mr-3 pt-2"><i class="far fa-clock text-primary mr-1"></i>{{ Str::substr($list->event->date, 5, 2) }}/{{ Str::substr($list->event->date, 8, 2) }} {{ $list->event->begin_time }}〜{{ $list->event->end_time }}</li>
-            <li class="list-unstyled pt-1"><i class="fas fa-map-marker-alt text-danger mr-2"></i>{{$list->event->address }}</li>
-            <li class="pt-1 list-unstyled"><i class="fas fa-user-friends mr-1 text-dark"></i>{{ $list->event->group }}</li>
-            <li class="list-unstyled mt-1">
-                <ul class="list-unstyled">
-                    <li class="font-weight-bold text-right mt-1"><i class="fas fa-users text-dark"></i>
-                        ＋{{ $list->diff }}人
-                </ul>
-            </li>
-        </ul>
-        @endforeach
+        <div class="event-card mb-4">
+            @foreach($lists as $list)
+            <ul class="list-unstyled border-bottom pb-4 pt-4 pl-4 pr-4 m-0">
+                <li class="font-weight-bold card-title"><a href="{{ $list->event->url }}" target="_blank" class="text-body">{{ $list->event->title }}</a></li>
+                <li class="list-unstyled mr-3 pt-2"><i class="far fa-clock text-primary mr-1"></i>{{ Str::substr($list->event->date, 5, 2) }}/{{ Str::substr($list->event->date, 8, 2) }} {{ $list->event->begin_time }}〜{{ $list->event->end_time }}</li>
+                <li class="list-unstyled pt-1"><i class="fas fa-map-marker-alt text-danger mr-2"></i>{{$list->event->address }}</li>
+                <li class="pt-1 list-unstyled"><i class="fas fa-user-friends mr-1 text-dark"></i>{{ $list->event->group }}</li>
+                <li class="list-unstyled mt-1">
+                    <ul class="list-unstyled">
+                        <li class="font-weight-bold text-right mt-1"><i class="fas fa-users text-dark"></i>
+                            ＋{{ $list->diff }}人
+                    </ul>
+                </li>
+            </ul>
+            @endforeach
+        </div>
     </div>
     <div class="offset-md-1 p-0 col-md-4 justify-content-center mt-4 mb-4">
-        <h2 class="title pb-2 title-border mt-2">ランキング</h2>
-        <div>
+        <h2 class="home-title-second text-white font-weight-bold mt-2">ランキング</h2>
+        <div class="bg-white pl-3 pr-3 pb-4 pt-3 shadow-sm">
             <h3 class="rank-title pb-2">人気イベントランキング</h3>
             @foreach($events as $i => $event)
             <ul class="d-flex list-unstyled border-bottom pb-3">
                 <li class="number-{{ $i+1 }} mr-1">{{ $i+1 }}</li>
-                <li class="ml-2 text-dark event-title"><a href="{{ $event->url }}" target="_blank" class="text-dark">{{ $event->title }}</a></li>
+                <li class="ml-2 event-title"><a href="{{ $event->url }}" target="_blank" class="text-dark">{{ $event->title }}</a></li>
             </ul>
             @endforeach
             <div class="text-right text-primary mb-4">
@@ -57,7 +59,7 @@
             @foreach($php_events as $i => $php_event)
             <ul class="d-flex list-unstyled border-bottom pb-3">
                 <li class="number-{{ $i+1 }} mr-1">{{ $i+1 }}</li>
-                <li class="ml-2 text-dark event-title"><a href="{{ $php_event->url }}" target="_blank" class="text-dark">{{ $php_event->title }}</a></li>
+                <li class="ml-2 event-title"><a href="{{ $php_event->url }}" target="_blank" class="text-dark">{{ $php_event->title }}</a></li>
             </ul>
             @endforeach
             <div class="text-right text-primary">
