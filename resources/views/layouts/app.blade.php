1a0b838a0bd4ef18b1c99beeac4b8256ec4b5641
chikamasuda <c.m.062820@gmail.com>
2022-01-26 23:20:14 +0900

    バージョンアップ

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
         <small class="text-center">2022 Connpass Summary</small>
     </footer>
   
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script src="{{ mix('js/app.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
